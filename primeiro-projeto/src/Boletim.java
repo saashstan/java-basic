df7b0188ca65631484a3e9c59a01cfb84ff525ea
Helena <helenadesouza22@gmail.com>
2023-08-09 15:24:26 -0300

    boletim

---
+++ primeiro-projeto/src/Boletim.java
@@ -0,0 +1,12 @@
+public class Boletim {
+    
+    public static void main (String [] args) {
+        int mediaFinal = 7;
+        if (mediaFinal<6)
+            System.out.println("Você foi reprovado!");
+        else if(mediaFinal==6)
+            System.out.println("Necessita fazer prova minerva.");
+        else
+            System.out.println("Você foi aprovado!");
+    }
+}
--- primeiro-projeto/src/MinhaClasse.java
+++ primeiro-projeto/src/MinhaClasse.java
@@ -1,4 +1,4 @@
-public class App {
+public class MinhaClasse {
     public static void main(String[] args) {
         String primeiroNome = "Alex";
         String segundoNome = "Souza";
