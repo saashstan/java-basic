cd27c6ef91ccbd3ef0fb1ee01ded78693b325243
Helena <helenadesouza22@gmail.com>
2023-08-09 15:09:59 -0300

    nome completo

--- primeiro-projeto/src/App.java
+++ primeiro-projeto/src/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Bem vindo ao meu Primeiro Projeto!");
+    public static void main(String[] args) {
+        String primeiroNome = "Alex";
+        String segundoNome = "Souza";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
