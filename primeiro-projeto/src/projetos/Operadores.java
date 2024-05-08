0438ddf096fd1e4d949647f328784ef116b1e223
Helena de Souza Mendonça <129570535+saashstan@users.noreply.github.com>
2024-05-08 13:04:37 +0000

    git pull

--- primeiro-projeto/src/projetos/Operadores.java
+++ primeiro-projeto/src/projetos/Operadores.java
@@ -2,9 +2,8 @@ public class Operadores {
     public static void main(String[] args) {
         int numero = 5;
 
-        numero = - numero;
+        numero = numero + 1;
 
         System.out.println(numero);
     }
-    
 }
