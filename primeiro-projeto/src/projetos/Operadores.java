c4d9f016a9e1a9e98a11d7eab2ae43041fc04d33
Helena de Souza Mendonça <129570535+saashstan@users.noreply.github.com>
2024-05-10 13:47:33 +0000

    GIT PULL

--- primeiro-projeto/src/projetos/Operadores.java
+++ primeiro-projeto/src/projetos/Operadores.java
@@ -1,8 +1,18 @@
 public class Operadores {
-    public static void main(String[] args) {
-        int numero = 5;
+   //public static void main(String[] args) {
+        //int numero = 5;
 
-        numero ++;
-        System.out.println(numero);
+        //numero ++;
+        //System.out.println(numero);
+    //}
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso" ;
+        
+        System.out.println(resultado);
     }
 }
---
+++ primeiro-projeto/src/projetos/Operadores2.java
@@ -0,0 +1,10 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numeroDois?" + simNao);
+    }
+}
